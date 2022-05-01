15480a06f04d6829bd898a51c9fce9fc6c312eb4
Pablo Velásquez Rivas <pablovelasquezr@gmail.com>
2022-05-01 02:14:18 -0400

    Add controllers, Update models, functional login

---
+++ src/main/java/com/findngo/findngo/controllers/AuthController.java
@@ -0,0 +1,31 @@
+package com.findngo.findngo.controllers;
+
+import com.findngo.findngo.dao.UsuarioDao;
+import com.findngo.findngo.models.Usuario;
+import com.findngo.findngo.utils.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    private UsuarioDao usuarioDao;
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @RequestMapping(value = "api/login", method = RequestMethod.POST)
+    public String login(@RequestBody Usuario usuario) {
+
+        Usuario usuarioLogueado = usuarioDao.obtenerUsuarioPorCredenciales(usuario);
+        if (usuarioLogueado != null) {
+            return jwtUtil.create(String.valueOf(usuarioLogueado.getId()), usuarioLogueado.getCorreo());
+        }
+        return "FAIL";
+    }
+
+}
--- src/main/java/com/findngo/findngo/models/Ciudad.java
+++ src/main/java/com/findngo/findngo/models/Ciudad.java
@@ -1,5 +1,8 @@
 package com.findngo.findngo.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -8,48 +11,16 @@ import java.util.Set;
 @Table(name = "ciudad")
 public class Ciudad {
     @Id
-    @Column(name = "ID_CIUDAD", nullable = false)
+    @Getter @Setter @Column(name = "ID_CIUDAD", nullable = false)
     private Integer id;
 
-    @Column(name = "NOMBRE", nullable = false)
+    @Getter @Setter @Column(name = "NOMBRE", nullable = false)
     private String nombre;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "ID_REGION", nullable = false)
+    @Getter @Setter @JoinColumn(name = "ID_REGION", nullable = false)
     private Region idRegion;
 
-    @OneToMany(mappedBy = "idCiudad")
+    @Getter @Setter @OneToMany(mappedBy = "idCiudad")
     private Set<Comuna> comunas = new LinkedHashSet<>();
-
-    public Set<Comuna> getComunas() {
-        return comunas;
-    }
-
-    public void setComunas(Set<Comuna> comunas) {
-        this.comunas = comunas;
-    }
-
-    public Region getIdRegion() {
-        return idRegion;
-    }
-
-    public void setIdRegion(Region idRegion) {
-        this.idRegion = idRegion;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
 }
